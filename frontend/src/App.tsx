8ffcd6dcc425f0c7f728693c5d2344e3c3c00458
aleksa <aleksa.ilic.dv@outlook.com>
2026-09-12 12:06:23 +0200

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>productlens</h1>;
+}
+
+export default App;
---
+++ frontend/src/main.tsx
@@ -0,0 +1,27 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import './index.css';
+import App from './App.tsx';
+
+const router = createBrowserRouter([{ path: '/', element: <App /> }]);
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000,
+    },
+  },
+});
+
+// biome-ignore lint/style/noNonNullAssertion: #root is defined in index.html
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  </StrictMode>,
+);
